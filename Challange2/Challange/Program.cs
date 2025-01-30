66e97384012d08946789cece1619f71b7c523072
Hüseyin Ardıl Karasungur <72706704+karasungurhuseyinardil@users.noreply.github.com>
2025-01-30 09:50:31 +0300

    Update Program.cs

--- Challange2/Challange/Program.cs
+++ Challange2/Challange/Program.cs
@@ -38,28 +38,3 @@ class Program
         return defaultOutputs[mod3];
     }
 }
-
-
-//{
-//        for (int i = 1; i <= 100; i++)
-//        {
-//            string output = GetOutput(i);
-//            Console.WriteLine($"{i}:{output}");
-
-//        }
-//    }
-//    static string GetOutput(int num)
-//    {
-//        string[] uc = ["", "Veni"];
-//        string[] ucbes = ["", "Vidi"];
-//        string[] ucbesdokuz = ["", "Vici"];
-//        string result = "";
-//        int sonuc = Convert.ToInt16((num % 3 == 0));
-//        result += uc[sonuc];
-//        sonuc = Convert.ToInt16((num % 3 == 0 && num % 5 == 0));
-//        result += ucbes[sonuc];
-//        sonuc = Convert.ToInt16((num % 3 == 0 && num % 5 == 0 && num % 9 == 0));
-//        result += ucbesdokuz[sonuc];
-//        return result;
-//    }
-//}
